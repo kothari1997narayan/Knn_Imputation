c6cce46767d09a04cf815db608279d44400865e6
kothari1997narayan <kothari1997narayan@gmail.com>
2021-02-12 22:26:01 +0530

    weighted Knn-Imputation

--- knn_imputation_weighted.py
+++ knn_imputation_weighted.py
@@ -25,6 +25,19 @@ def nan_distance(x,y):
     dist = dist*tot_cand/pres_cand
     return dist**0.5
 
+def nan_matrix(data):
+    n_row = data.shape[0]
+    dist_matrix = np.zeros((n_row,n_row))
+    for i in range(n_row):
+        for j in range(n_row):
+            if(j==i):
+                continue
+            elif(j<i):
+                dist_matrix[i,j] = dist_matrix[j,i]
+            else:
+                dist_matrix[i,j] = nan_distance(data[i],data[j])
+    return(dist_matrix)
+            
 
 def knn_imputation(data,n_neighbors = 2):
     count = 0
@@ -72,23 +85,26 @@ def optimized(data,n_neighbors = 2):
     indexes_nan = np.argwhere(np.isnan(data))
     final_data = data.copy()
     ## it will produce a distance matrix with row in the dataset
-    dist = nan_euclidean_distances(data)
+    dist = nan_matrix(data)
     for index in indexes_nan:
         print(count)
         count = count + 1 
         row = index[0]
         col = index[1]
         ## Sorting the distance matrix for the particular row and getting top n values
-        sort_indxs = dist[row].argsort()
-        close_val = []
-        for i in sort_indxs:
-            if(np.isnan(data[i,col])):
-                continue
-            close_val.append(data[i,col])
-            if(len(close_val)==n_neighbors):
-                break
-        adjusted_value = np.mean(close_val)
-        final_data[row][col] = adjusted_value
+    sort_indxs = dist[row].argsort()
+    close_val = []
+    total_dist = 0
+    for i in sort_indxs:
+        if(np.isnan(data[i,col])):
+            continue
+        inv_dist = 1/dist[row,i]
+        total_dist = total_dist + inv_dist
+        close_val.append(data[i,col]*inv_dist)
+        if(len(close_val)==n_neighbors):
+            break
+    adjusted_value = np.sum(close_val)/total_dist
+    final_data[row][col] = adjusted_value
     return(final_data)
     
 final = optimized(data,5)
